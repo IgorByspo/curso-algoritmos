3ea6ef48cfceca18500181ee869d388d37b75563
IgorByspo <87457315+IgorByspo@users.noreply.github.com>
2021-08-04 15:24:41 -0300

    acima_diagonal.c
    O algoritmo funcionava, mas estava com um estrutura if-else que não havia na resolução deste mesmo exercício no Visualg.

--- c/acima_diagonal.c
+++ c/acima_diagonal.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 
-int main(){
-    int n, somaAcima;
+int main() {
 
-    printf("Qual a ordem da matriz? ");
+    int n, i, j, somaAcima;
+
+    printf("Qual e a ordem da matriz? ");
     scanf("%d", &n);
 
-    int matriz[n][n];
+    int mat[n][n];
 
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-			printf("Elemento [%d,%d]: ", i, j);
-            scanf("%d", &matriz[i][j]);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("Elemento [%d,%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
         }
     }
-
-	somaAcima = 0;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<n; j++) {
-            if (i < j) {
-                somaAcima = somaAcima + matriz[i][j];
-            }
+    
+    somaAcima = 0;
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            somaAcima = somaAcima + mat[i][j];
         }
     }
 
     printf("SOMA DOS ELEMENTOS ACIMA DA DIAGONAL PRINCIPAL = %d\n", somaAcima);
 
     return 0;
+
 }
